9c22316ce841a8060610d0fdbea2d9aa5a63afaa
olumideadebayo <olumide.adebayo@gmail.com>
2023-01-10 17:05:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit
 
-streamlit.title('My Parent''s New Healthy Diner')
+streamlit.title("My Parent's New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
