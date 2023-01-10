e72d32f9922fdb7e08cff14580c80ac2e4f9a714
olumideadebayo <olumide.adebayo@gmail.com>
2023-01-10 17:12:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,9 @@ streamlit.header('Build Your Own Fruit Smoothie')
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
-my_fruit_list = my_fruit_list.set_index('fruit')
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 #pick list
-streamlit.multiselect("Pick some fruites:",list(my_fruit.list.index))
+#streamlit.multiselect("Pick some fruits:",list(my_fruit.list.index))
 
 #display
 streamlit.dataframe(my_fruit_list)
