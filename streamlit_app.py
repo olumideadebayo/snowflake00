32edd3f194774f2e70fa15c163e36fbf6758292d
olumideadebayo <olumide.adebayo@gmail.com>
2023-01-12 13:37:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-  if not fruit_choice:
+  if not fruit_choice | len(fruit_choice) < 2 :
     streamlit.write('The user entered ', fruit_choice)
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
